8819e2935409ca33c8e2d2b83faf86543b53b6b1
branzinho <branzinho9@gmail.com>
2024-05-11 13:50:00 -0700

    Added remaining app properties

--- backend/src/config/app-config.service.ts
+++ backend/src/config/app-config.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Hash } from 'viem';
+import { Chain, base, baseSepolia } from 'viem/chains';
+
+const chains = {
+  base,
+  baseSepolia,
+};
 
 @Injectable()
 export class AppConfig {
   public l2Rpc: string;
-  public l2Chain: string;
+  public l2Chain: Chain;
+  public signerKey: Hash;
+  public appSignerName: string;
+  public appSignerVersion: string;
 
   constructor(private readonly configService: ConfigService) {
     this.l2Rpc = this.configService.get('L2_RPC_URL');
-    this.l2Chain = this.configService.get('L2_CHAIN');
+    this.l2Chain = chains[this.configService.get('L2_CHAIN')];
+    this.signerKey = this.configService.get('SIGNER_KEY');
+    this.appSignerName = this.configService.get('APP_SIGNER_NAME');
+    this.appSignerVersion = this.configService.get('APP_SIGNER_VERSION');
   }
 }
