331d27610ce8f91c145590333e1ccd185846ccce
branzinho <branzinho9@gmail.com>
2024-05-14 21:47:17 -0700

    Added listing retrieval

--- backend/src/listed-names/listed-names.service.ts
+++ backend/src/listed-names/listed-names.service.ts
@@ -4,6 +4,10 @@ import listings from 'src/listed-names/listings.json';
 
 @Injectable()
 export class ListedNamesService {
+  public async getListings(): Promise<NameListing[]> {
+    return listings as NameListing[];
+  }
+
   public async getNameListing(name: string): Promise<NameListing> {
     return listings.find((l) => l.name === name) as NameListing;
   }
--- frontend-app/src/components/mint-form/MintFormContainer.tsx
+++ frontend-app/src/components/mint-form/MintFormContainer.tsx
@@ -1,10 +1,46 @@
-import { Card, Typography, Button } from "@ensdomains/thorin";
+import {
+  Button,
+  Card,
+  EnsSVG,
+  Input,
+  MagnifyingGlassSimpleSVG,
+  Typography,
+} from "@ensdomains/thorin";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface Listing {
+  name: string;
+}
 
 export const MintFormContainer = () => {
-    return <Card>
-        <ConnectButton/>
-        <Typography>Find your name</Typography>
-        <Button>Click</Button>
+  const [listings, setListings] = useState<Listing[]>();
+
+  useEffect(() => {
+    getListings();
+  });
+
+  function getListings() {
+    axios
+      .get("http://localhost:3000/l2/listings")
+      .then((resp) => setListings(resp.data))
+      .catch((err) => {
+        console.log(err.response.data.error[0].message);
+      });
+  }
+
+  return (
+    <Card>
+      <ConnectButton />
+      <Typography>Find your name</Typography>
+      <Input
+        icon={<MagnifyingGlassSimpleSVG />}
+        label="ENS Name"
+        placeholder="0xA0Cf…251e"
+        prefix={<EnsSVG />}
+      />
+      <Button>Click</Button>
     </Card>
-}
+  );
+};
