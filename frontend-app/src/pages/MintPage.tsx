84aae6bf953f6d9b72fa20223cd874e89ab16ac8
nenadmitt <nenadmitt@gmail.com>
2024-05-25 22:40:35 +0200

    set success screen

--- frontend-app/src/pages/MintPage.tsx
+++ frontend-app/src/pages/MintPage.tsx
@@ -1,6 +1,10 @@
-import { useParams } from "react-router-dom"
-import { ChangeMintNetwork, MintSubnameForm, ScreenContainer } from "../components";
-import { Card } from "@ensdomains/thorin";
+import { useParams } from "react-router-dom";
+import {
+  ChangeMintNetwork,
+  MintSubnameForm,
+  ScreenContainer,
+} from "../components";
+import { Card} from "@ensdomains/thorin";
 import { useEffect, useState } from "react";
 import "./MintPage.css";
 import { getSingleListing } from "../api";
@@ -8,38 +12,44 @@ import { Listing } from "../api/types";
 import { useWeb3Network } from "../web3";
 
 export const MintPage = () => {
+  const { parentName } = useParams();
+  const [listing, setListing] = useState<{
+    isFetching: boolean;
+    item?: Listing;
+  }>({
+    isFetching: true,
+  });
+  const { networkName } = useWeb3Network();
 
-    const { parentName } = useParams();
-    const [listing, setListing] = useState<{
-        isFetching: boolean
-        item?: Listing
-    }>({
-        isFetching: true,
-    })
-    const { networkName } = useWeb3Network();
+  useEffect(() => {
+    getSingleListing(parentName as string).then((res) => {
+      setListing({
+        isFetching: false,
+        item: res,
+      });
+    });
+  }, []);
 
-    useEffect(() => {
+  if (listing.isFetching || !listing.item) {
+    return <ScreenContainer isLoading={true} />;
+  }
 
-        getSingleListing(parentName as string).then(res => {
-            setListing({
-                isFetching: false,
-                item: res
-            })
-        })
-    }, [])
+  const isProperNetwork = listing.item.network === networkName;
 
-    if (listing.isFetching || !listing.item) {
-        return <ScreenContainer isLoading={true} />
-    }
-
-    const isProperNetwork = listing.item.network === networkName;
-
-    return <ScreenContainer>
-        <div className="mint-page">
-            <Card className="mint-page-container">
-                {!isProperNetwork && <ChangeMintNetwork requiredNetwork={listing.item.network} />}
-                {isProperNetwork && <MintSubnameForm parentName={parentName as string} />}
-            </Card>
-        </div>
+  return (
+    <ScreenContainer>
+      <div className="mint-page">
+        <Card className="mint-page-container">
+          <>
+            {!isProperNetwork && (
+              <ChangeMintNetwork requiredNetwork={listing.item.network} />
+            )}
+            {isProperNetwork && (
+              <MintSubnameForm parentName={parentName as string} />
+            )}
+          </>
+        </Card>
+      </div>
     </ScreenContainer>
-}
+  );
+};
