5b0a1dfa2a40769806fa7354451b134ecb951113
branzinho <branzinho9@gmail.com>
2024-05-26 19:04:14 -0700

    Load metadata controller

--- backend/src/metadata/metadata.module.ts
+++ backend/src/metadata/metadata.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AppConfigModule } from 'src/config/config.module';
 import { MetadataService } from './metadata.service';
 
 @Module({
   providers: [MetadataService],
   exports: [MetadataService],
+  imports: [AppConfigModule],
 })
 export class MetadataModule {}
