9971ad10fad8f7154c9633bad3996777422b458c
nenadmitt <arthy@namespace.tech>
2024-06-25 23:40:37 +0200

    add temp metadata

---
+++ backend/src/web3/temp-metadata.ts
@@ -0,0 +1,80 @@
+import {
+  Controller,
+  Get,
+  Injectable,
+  OnModuleInit,
+  Param,
+} from '@nestjs/common';
+import { Address, namehash, parseAbiItem } from 'viem';
+import { RpcClient } from './rpc-client';
+import { Network } from 'src/dto/types';
+import { getContracts } from './contracts/contract-addresses';
+
+const DEFAULT_IMAGE = "https://namespace.fra1.cdn.digitaloceanspaces.com/misc/basedsummer.png";
+
+@Injectable()
+export class MetadataService implements OnModuleInit {
+  private cache: Record<string, { name: string; avatar: string }> = {};
+
+  constructor(private readonly clients: RpcClient) {}
+
+  async onModuleInit() {
+    const { controller, resolver } = getContracts('base');
+    const client = this.clients.getPublicClient('base');
+    const logs = await client.getLogs({
+      address: controller,
+      event: parseAbiItem(
+        'event NameMinted(string label,string parentLabel,bytes32 subnameNode,bytes32 parentNode,address owner,uint256 price,uint256 fee,address paymentReceiver)',
+      ),
+      fromBlock: BigInt(12353354 - 1000),
+    });
+
+    const _tempCache = {};
+    for (const log of logs) {
+      const { label, parentLabel } = log.args;
+      const fullName = `${label}.${parentLabel}.eth`;
+      const hash = namehash(fullName);
+      let avatar = '';
+
+      try {
+        avatar = await client.readContract({
+          abi: [parseAbiItem('function text(bytes32,string) returns(string)')],
+          address: resolver,
+          args: [hash, 'avatar'],
+          functionName: 'text',
+        });
+      } catch (err) {
+        console.error(err, "ERROR FETCHING AVATAR");
+      }
+      _tempCache[hash] = {
+        name: fullName,
+        description: "Based subname!",
+        image: avatar || DEFAULT_IMAGE,
+      }
+
+    }
+  }
+
+  public async resolve(token: string, network: string) {
+    if (network === "base") {
+        return this.cache[token]
+    }
+    return {
+        name: "Based subname",
+        image: DEFAULT_IMAGE
+    }
+  }
+}
+
+@Controller('/ens')
+export class TempMetadataController {
+  constructor(private readonly metadataService: MetadataService) {}
+
+  @Get('/:network/:tokenId')
+  public async resolve(
+    @Param('network') network: string,
+    @Param('tokenId') token: string,
+  ) {
+    return this.metadataService.resolve(token, network);
+  }
+}
--- backend/src/web3/web3.module.ts
+++ backend/src/web3/web3.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppConfigModule } from 'src/config/config.module';
 import { NameRegistryService } from './contracts/name-registry.service';
 import { RpcClient } from './rpc-client';
+import { MetadataService, TempMetadataController } from './temp-metadata';
 
 @Module({
   imports: [AppConfigModule],
-  providers: [RpcClient, NameRegistryService],
-  exports: [RpcClient, NameRegistryService],
+  providers: [RpcClient, NameRegistryService, MetadataService],
+  controllers: [TempMetadataController],
+  exports: [RpcClient, NameRegistryService, MetadataService],
 })
 export class Web3Module {}
